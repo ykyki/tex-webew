0c5e1ca549e84d8c79736cca525c1912d103c00a
ykyki <bsyki@pm.me>
2023-06-27 16:25:29 +0900

    apply prettier

--- webfront3/next.config.js
+++ webfront3/next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  basePath: "/tex-webew",
-  assetPrefix: "/tex-webew/",
-  webpack: (config, { isServer }) => {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    config.output.webassemblyModuleFilename =
-      (isServer ? "../" : "") + "static/wasm/webassembly.wasm";
-    return config;
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    basePath: '/tex-webew',
+    assetPrefix: '/tex-webew/',
+    webpack: (config, { isServer }) => {
+        config.experiments = {
+            asyncWebAssembly: true,
+            layers: true,
+        };
+        config.output.webassemblyModuleFilename =
+            (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
+        return config;
+    },
 };
 
 module.exports = nextConfig;
