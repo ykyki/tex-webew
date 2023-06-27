6abe4c19da19c1e2d9725fb444e3838503643edd
ykyki <bsyki@pm.me>
2023-06-27 12:36:22 +0900

    add wasm_rs to webfront3

--- webfront3/next.config.js
+++ webfront3/next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   swcMinify: true,
   basePath: "/tex-webew",
   assetPrefix: "/tex-webew/",
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    config.output.webassemblyModuleFilename =
+      (isServer ? "../" : "") + "static/wasm/webassembly.wasm";
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- webfront3/pages/index.tsx
+++ webfront3/pages/index.tsx
@@ -1,5 +1,12 @@
+import { parse_paragraphs } from "wasm_rs";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  console.log(parse_paragraphs("Hello, world!$X$, $$Y$$."));
+  return (
+    <div>
+      Home
+      <p>{JSON.stringify(parse_paragraphs("Hello, world!$X$, $$Y$$."))}</p>
+    </div>
+  );
 }
