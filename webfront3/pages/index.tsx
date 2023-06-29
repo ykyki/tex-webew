20c4c21cd2f6a3fb6f3cdca2e16a905f428e39c3
ykyki <bsyki@pm.me>
2023-06-29 23:06:32 +0900

    set two side layout

--- webfront3/pages/index.tsx
+++ webfront3/pages/index.tsx
@@ -2,10 +2,12 @@ import { ParseResult, parse_paragraphs } from '@lib/parser';
 import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import Container from '@components/container';
 import Meta from '@components/meta';
+import styles from 'styles/two-column.module.css';
 
 export default function Home() {
     const initialText = `\\( \\mathscr{V} := U_x^X \\)は\\( X \\)の開被覆である.
-よって, \\( \\mathscr{V} := U_x^X \\)は\\( X \\)の開被覆である.`;
+よって, \\( \\mathscr{V} := U_x^X \\)は\\( X \\)の開被覆である.
+`.repeat(100);
 
     const [text, setText] = useState(initialText);
     const textAreaHandler = useCallback(
@@ -26,21 +28,29 @@ export default function Home() {
     }, [text]);
 
     return (
-        <Container>
+        <Container large>
             <Meta />
 
             <h1>tex web view</h1>
-            <Container>
-                <textarea
-                    value={text}
-                    onChange={textAreaHandler}
-                    style={{ width: '100%', height: '300px' }}
-                />
-                <span>count: {charCount}</span>
-            </Container>
-            <Container>
-                <p>{JSON.stringify(parseResult)}</p>
-            </Container>
+            <div className={styles.two_column}>
+                <div className={styles.left_side}>
+                    <Container>
+                        <textarea
+                            value={text}
+                            onChange={textAreaHandler}
+                            style={{ width: '100%', height: '300px' }}
+                        />
+                        <span>count: {charCount}</span>
+                    </Container>
+                </div>
+                <div className={styles.right_side}>
+                    <Container>
+                        <p style={{ wordBreak: 'break-word' }}>
+                            {JSON.stringify(parseResult)}
+                        </p>
+                    </Container>
+                </div>
+            </div>
         </Container>
     );
 }
