998dbf8fbb37ff5ca87d706e0346de0b2d3ea68d
ykyki <bsyki@pm.me>
2023-06-29 21:20:07 +0900

    create parseResult schemas

--- webfront3/lib/parser.ts
+++ webfront3/lib/parser.ts
@@ -1,31 +1,67 @@
-import { unknown, z } from 'zod';
+import { z } from 'zod';
 import { parse_paragraphs as w_parse_paragraphs } from 'wasm_rs';
 
-const apiStatusOk = z
+export const parse_paragraphs = (
+    input: string,
+): z.infer<typeof parseResult> => {
+    const result = w_parse_paragraphs(input);
+    return parseResult.parse(result);
+};
+
+const evParagraphs = z.object({
+    kind: z.literal('paragraphs'),
+    keys: z.array(z.string()),
+});
+const evParagraph = z.object({
+    kind: z.literal('paragraph'),
+    keys: z.array(z.string()),
+});
+const evText = z.object({
+    kind: z.literal('text'),
+    content: z.string(),
+});
+const evInlineCommand = z.object({
+    kind: z.literal('inline_command'),
+    content: z.string(),
+});
+const evInlineMath = z.object({
+    kind: z.literal('inline_math'),
+    status: z.union([z.literal('ok'), z.literal('error')]),
+    content: z.string(),
+});
+const evDisplayMath = z.object({
+    kind: z.literal('display_math'),
+    status: z.union([z.literal('ok'), z.literal('error')]),
+    content: z.string(),
+});
+
+const entryValue = z.union([
+    evParagraphs,
+    evParagraph,
+    evText,
+    evInlineCommand,
+    evInlineMath,
+    evDisplayMath,
+]);
+
+const entry = z.object({
+    key: z.string(),
+    value: entryValue,
+});
+
+const parseResultOk = z
     .object({
         status: z.literal('ok'),
         root: z.string(),
-        map: z.array(
-            z.object({
-                key: z.string(),
-                value: z.object({
-                    kind: z.string(),
-                }),
-            }),
-        ),
+        map: z.array(entry),
     })
     .strict();
 
-const apiStatusError = z
+const parseResultError = z
     .object({
         status: z.literal('error'),
         message: z.string(),
     })
     .strict();
 
-const apiResult = z.union([apiStatusOk, apiStatusError]);
-
-export const parse_paragraphs = (input: string): z.infer<typeof apiResult> => {
-    const result = w_parse_paragraphs(input);
-    return apiResult.parse(result);
-};
+const parseResult = z.union([parseResultOk, parseResultError]);
