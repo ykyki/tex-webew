fd3646322306b4cfb570bcb848930633f694c73d
ykyki <bsyki@pm.me>
2023-06-29 21:33:42 +0900

    add textarea

--- webfront3/pages/index.tsx
+++ webfront3/pages/index.tsx
@@ -1,14 +1,31 @@
 import { parse_paragraphs } from '@lib/parser';
-import React from 'react';
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 
 export default function Home() {
-    console.log(parse_paragraphs('Hello, world!$X$, $$Y$$.'));
+    const initialText = `\\( \\mathscr{V} := U_x^X \\)は\\( X \\)の開被覆である.
+
+よって, \\( \\mathscr{V} := U_x^X \\)は\\( X \\)の開被覆である.`;
+
+    const [text, setText] = useState(initialText);
+
+    const textAreaHandler = useCallback(
+        (e: ChangeEvent<HTMLTextAreaElement>) => {
+            setText(e.target.value);
+        },
+        [],
+    );
+
+    useEffect(() => {
+        console.log(text);
+    }, [text]);
+
     return (
         <div>
             Home
             <p>
                 {JSON.stringify(parse_paragraphs('Hello, world!$X$, $$Y$$.'))}
             </p>
+            <textarea value={text} onChange={textAreaHandler} />
         </div>
     );
 }
