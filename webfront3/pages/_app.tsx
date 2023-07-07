e8c1cbe51ddb77354814028243029e055101b201
ykyki <bsyki@pm.me>
2023-07-08 00:00:12 +0900

    🆕create _app.tsx

---
+++ webfront3/pages/_app.tsx
@@ -0,0 +1,19 @@
+// import { Profiler } from 'react';
+import type { AppProps } from 'next/app';
+
+function App({ Component, pageProps }: AppProps) {
+    return (
+        // <Profiler
+        //     id="app"
+        //     onRender={(id, phase, actualDuration) => {
+        //         console.log(
+        //             `Component "${id}" took ${actualDuration}ms to render(${phase}).`,
+        //         );
+        //     }}>
+        //     <Component {...pageProps} />
+        // </Profiler>
+        <Component {...pageProps} />
+    );
+}
+
+export default App;
